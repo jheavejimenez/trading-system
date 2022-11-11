9f17feca56e19752e8237fc13ff28356e7fff932
jheave <jimenezjheave123@gmail.com>
2022-11-11 13:47:06 +0800

    feat: trading stocks

--- trading/models.py
+++ trading/models.py
@@ -82,7 +82,7 @@ class Trade(models.Model):
         default='buy',
     )
     quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- trading/serializer.py
+++ trading/serializer.py
@@ -65,6 +65,30 @@ class TradeSerializer(BaseSerializer):
 
 
 class TradeInsertSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        context = kwargs.get('context', {})
+        self.request = context.get('request', None)
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = Trade
         fields = "__all__"
+        read_only_fields = ['total']
+
+    def create(self, validated_data):
+        user = self.request.user
+        wallet = Wallet.objects.get(user=user)
+        if wallet.balance < validated_data['quantity'] * validated_data['stock'].price:
+            raise serializers.ValidationError('Insufficient funds')
+        wallet.balance -= validated_data['quantity'] * validated_data['stock'].price
+        wallet.save()
+
+        total_value = validated_data['quantity'] * validated_data['stock'].price
+        trade = Trade.objects.create(
+            user=user,
+            stock=validated_data['stock'],
+            quantity=validated_data['quantity'],
+            total=total_value
+        )
+        return trade
